f2a0dfca3123504fcd7ecbc7f191b5a7a183eb81
joakinGH <joakin.balsera@gmail.com>
2014-12-07 16:45:50 +0100

    "cacheSolve" function is done.

--- cachematrix.R
+++ cachematrix.R
@@ -25,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 #
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
